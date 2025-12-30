d99282702341d7dee1fb9f442411a18d26988fe7
10ya54 <840shikamaru@gmail.com>
2025-12-30 11:35:58 +0900

    first commit

---
+++ src/main/java/com/example/gratitude_share_app/GratitudeShareAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.gratitude_share_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratitudeShareAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratitudeShareAppApplication.class, args);
+	}
+
+}
